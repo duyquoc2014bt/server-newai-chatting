022622ef3db21c2ca9c763ac5d1c54aee9b11aac
duyquoc2014bt <duyquoc2014bt@gmail.com>
2025-02-18 23:06:42 +0700

    ka

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,8 +8,7 @@ import { ChatSocket } from './app/sockets/chat.socket';
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: '*' },
-  addTrailingSlash: false,
+  cors: { origin: '*' }
 });
 
 app.use(cors());
